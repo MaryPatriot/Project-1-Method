9cfb239432da000fda45ff852c674c3ae9a8ead9
Mary <mary.vatanparast@gmail.com>
2021-01-31 01:46:51 +0100

    Add project files.

---
+++ Project 1 Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************************************************************************************");
+            Console.WriteLine("Maryam Vatanparast");
+            Console.WriteLine("C# 1th project");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Type of vehicle?");
+            Console.WriteLine("1:Car");
+            Console.WriteLine("2: Truck");
+            Console.WriteLine("3: Bus");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
